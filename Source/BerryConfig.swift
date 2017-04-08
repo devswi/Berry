6be2c6d836d3b6e379b5aab74164d4e398aca571
ShiWeiCN <stayfocusjs@gmail.com>
2017-04-08 17:02:33 +0800

    Berry file refrence error

--- Source/BerryConfig.swift
+++ Source/BerryConfig.swift
@@ -10,54 +10,29 @@ import UIKit
 
 public class BerryConfig {
     
-    // MARK: - Menu title properties
-    struct BerryMenuProperty {
-        
-    }
+    public var menuProperty: BerryMenuProperty = BerryMenuProperty.default()
+    public var cellProperty: BerryCellProperty = BerryCellProperty.default()
+    public var arrowProperty: BerryArrowProperty = BerryArrowProperty.default()
+    public var maskProperty: BerryMaskProperty = BerryMaskProperty.default()
+    public var otherProperty: BerryOtherProperty = BerryOtherProperty.default()
     
-    // MARK: - Menu selection cell properties
-    struct BerryCellProperties {
-        var cellHeight: CGFloat = 44.0
-        var cellBackgroundColor: UIColor?
-        var cellSeparatorColor: UIColor?
-        var cellTextLabelColor: UIColor?
-        var cellTextLabelFont: UIFont = UIFont.systemFont(ofSize: 14)
-        var cellSelectedBackgroundColor: UIColor?
-        var cellSelectedTextLabelColor: UIColor?
-        var cellTextLabelAlignment: NSTextAlignment = .center
-    }
-    
-    // MARK: - Navigation bar arrow properties
-    struct BerryArrowProperties {
-        var arrowTineColor: UIColor?
-        var arrowPadding: CGFloat = 10.0
-        var arrowImage: UIImage?
-    }
-    
-    // MARK: - Mask properties
-    struct BerryMaskProperties {
-        var maskBackgroundColor: UIColor?
-        var maskBackgroundOpacity: CGFloat = 0.5
-    }
-    
-    // MARK: - Other properties
-    struct BerryOtherProperties {
-        var duration: TimeInterval = 0.375
-        var shouldChangeTitleText: Bool = true
-        var shouldKeepSelectedCellColor: Bool = true
-    }
-    
-    public class func config() -> BerryConfig {
-    
+    public class func `default`() -> BerryConfig {
         return BerryConfig()
     }
     
-    init() {
-        defaultConfig()
-    }
-    
-    fileprivate func defaultConfig() {
+    public class func config(menuProperty: BerryMenuProperty,
+                             cellProperty: BerryCellProperty,
+                             arrowProperty: BerryArrowProperty,
+                             maskProperty: BerryMaskProperty,
+                             otherProperties: BerryOtherProperty) -> BerryConfig {
         
+        let config = BerryConfig()
+        config.menuProperty = menuProperty
+        config.cellProperty = cellProperty
+        config.arrowProperty = arrowProperty
+        config.maskProperty = maskProperty
+        config.otherProperty = otherProperties
+        return config
     }
     
 }
